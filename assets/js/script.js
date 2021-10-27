3009fe2b3d25cd6c5188733b9780b54601479dc4
Eduardo <edu3839@hotmail.com>
2021-10-27 14:47:23 -0300

    [Feature Eduardo - Task: Update JavaScript | 25.10.21]

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,13 @@
+
 let nome = window.document.getElementById("nome")
 
 let email = document.querySelector("#email")
 
 let assunto = document.getElementById("assunto")
+let nomeOk = false
+let emailOk = false
+let assuntoOk = false
+let mapa = document.querySelector("#mapa")
 
 nome.style.width = "100%"
 email.style.width = "100%"
@@ -17,6 +22,7 @@ function validaNome() {
      else{
          txtNome.innerHTML = "Nome Válido"
          txtNome.style.collor = "green"
+         nomeOk = true
      }
 }
 function validaEmail(){
@@ -28,5 +34,34 @@ function validaEmail(){
     else{
         txtEmail.innerHTML = "Email Válido"
         txtEmail.style.collor = "green"
+        emailOk = true
     }
+}
+function validaAssunto(){
+    let txtAssunto = document.querySelector("#txtAssunto")
+    if(assunto.value.length >99){
+        txtAssunto.innerHTML = "Coloque no máximo 100 caracteres"
+        txtAssunto.style.collor = "red"
+    }
+    else{
+        txtAssunto.innerHTML = "Obrigado por sua opnião/sugestão"
+        txtAssunto.style.collor = "green"
+        assuntoOk = true
+    }
+}
+function enviar(){
+    if(nomeOk == true && emailOk == true && assuntoOk == true){
+        alert("Formulário enviado com sucesso!")
+    }
+    else{
+        alert("Preencha o formulário corretamente!")
+    }
+}
+function mapaZoom(){
+    mapa.style.width = "800px"
+    mapa.style.heigth = "600px"
+}
+function mapaNormal(){
+    mapa.style.width = "400px"
+    mapa.style.heigth = "300px"
 }
